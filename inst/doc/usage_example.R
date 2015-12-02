197b83714fdddb11a16996d50ce5c6771ecc33c0
SESman <yveslebras.fr@gmail.fr>
2015-12-02 15:19:56 +0100

    add a demo vignette

---
+++ inst/doc/usage_example.R
@@ -0,0 +1,37 @@
+## ------------------------------------------------------------------------
+require("rbl")
+require("eddies")
+
+ses <- readRDS("F:/data/ses/post_repro/rbl/large_acc/2011-28.rds")
+ses$stat$day <- floorPOSIXct(ses$stat$time)
+
+# Setup temporal subset
+days <- seq(
+  from = as.POSIXct("2011-11-25", tz = "UTC"), 
+  to = as.POSIXct("2011-12-12", tz = "UTC"), 
+  by = "day"
+  )
+
+# Before using the package make sure that the database path is correct
+# Consider setting this option in your .Rprofile if it is not the case
+options("eddies.path")
+
+# Setup geographical subset (Optional)
+# options("eddies.area") <- list(
+#   lon = range(ses$stat$lon), 
+#   lat = range(ses$stat$lat)
+#   )
+# Here we will use defaults
+options("eddies.area")
+
+# Extract matching eddies from the database
+edd <- eddies(days)
+
+## ------------------------------------------------------------------------
+names(edd) # See description of these variables in ?eddies
+
+cnd <- ses$stat$day %in% days
+plot(lat ~ lon, ses$stat[cnd, ], type = "n")
+plot(edd, cex = 0.5, add = TRUE)
+lines(lat ~ lon, ses$stat[cnd, ], lwd = 2)
+
